81384d3e298202c3ed21b21bdcbb37e1fcdda066
Ahd <dafuq.ahd@gmail.com>
2020-06-11 15:05:14 +0300

    featured items are now tapable that redirect to corresponding single screen

--- lib/components/featured.dart
+++ lib/components/featured.dart
@@ -1,3 +1,4 @@
+import 'package:fashionapp/screens/single.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedDesigns extends StatelessWidget {
@@ -49,77 +50,85 @@ class FeaturedCard extends StatelessWidget {
   const FeaturedCard({Key key, this.data}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 5, vertical: 20),
-          height: MediaQuery.of(context).size.width / 1.7,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.3),
-                spreadRadius: 2,
-                blurRadius: 7,
-                offset: Offset(0, 3), // changes position of shadow
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Single(data: data)),
+        );
+      },
+      child: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 5, vertical: 20),
+            height: MediaQuery.of(context).size.width / 1.7,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 2,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+              image: DecorationImage(
+                image: NetworkImage(data['imageurl']['en-US']),
+                fit: BoxFit.cover,
               ),
-            ],
-            image: DecorationImage(
-              image: NetworkImage(data['imageurl']['en-US']),
-              fit: BoxFit.cover,
             ),
           ),
-        ),
-        Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 2,
-                  vertical: 2,
-                ),
-                width: double.infinity,
-                child: Text(
-                  data['title']['en-US'],
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    color: Colors.grey[700],
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 2,
+                    vertical: 2,
+                  ),
+                  width: double.infinity,
+                  child: Text(
+                    data['title']['en-US'],
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 2,
-                  vertical: 2,
-                ),
-                width: double.infinity,
-                child: Text(
-                  data['type']['en-US'],
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    color: Colors.grey[700],
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 2,
+                    vertical: 2,
+                  ),
+                  width: double.infinity,
+                  child: Text(
+                    data['type']['en-US'],
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 2,
-                  vertical: 2,
-                ),
-                width: double.infinity,
-                child: Text(
-                  '\$${data['price']['en-US']}',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    color: Colors.grey[700],
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 2,
+                    vertical: 2,
+                  ),
+                  width: double.infinity,
+                  child: Text(
+                    '\$${data['price']['en-US']}',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
